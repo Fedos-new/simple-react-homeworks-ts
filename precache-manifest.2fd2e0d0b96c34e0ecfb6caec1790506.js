1fbecd3e7975d89ff391b2e7def08d165350f66a
Fedos-new <fedluky@gmail.com>
2020-11-05 14:11:56 +0700

    Updates

---
+++ precache-manifest.2fd2e0d0b96c34e0ecfb6caec1790506.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7438a7ff2e3d0951e33caf943f9357f3",
+    "url": "/simple-react-homeworks-ts/index.html"
+  },
+  {
+    "revision": "adca8d6534ceacf74b0d",
+    "url": "/simple-react-homeworks-ts/static/css/main.5e646f28.chunk.css"
+  },
+  {
+    "revision": "23eff46d425c9e6be58b",
+    "url": "/simple-react-homeworks-ts/static/js/2.7bf06a30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-homeworks-ts/static/js/2.7bf06a30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adca8d6534ceacf74b0d",
+    "url": "/simple-react-homeworks-ts/static/js/main.6bff5dae.chunk.js"
+  },
+  {
+    "revision": "cf6ef377e604236463a8",
+    "url": "/simple-react-homeworks-ts/static/js/runtime-main.7779e124.js"
+  }
+]);
